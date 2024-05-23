82c2a8c4a1afa1894f4b4b5533a5fac6493577cc
egmal <eg.malyn.com@gmail.com>
2024-05-23 09:42:59 +0300

    Осталось последнее задание(Спросить за 5.3 2)

---
+++ № 5.4 V10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __5._4_V10
+{
+    internal class Program
+    {
+        const double e = Math.E;
+        public static double F(double x) => Math.Sqrt(x)/(x+1);
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            double a, b, x, h, s;
+            int n;
+            Random random = new Random();
+
+            Console.WriteLine("Введите начало промежутка");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите конец промежутка");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите N");
+            n = int.Parse(Console.ReadLine());
+
+            h = (b - a) / n;
+            s = 0;
+            for (int i = 1; i < n; i++)
+            {
+                x = a + (b - a) * random.NextDouble();
+                s = s + F(x) * h;
+
+            }
+            Console.WriteLine("ЗНАЧЕНИЕ ИНТЕГРАЛА {0}", Math.Round(s, 5));
+            Console.ReadKey();
+
+
+        }
+    }
+}
--- №5.3 V10 2/Program.cs
+++ №5.3 V10 2/Program.cs
@@ -4,18 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _5._3_V10
+namespace _5._3_V10_2
 {
     internal class Program
     {
-        const double e = Math.E;
-        public static double F(double x, double a) => Math.Pow(e, a * x);
-
-
+        public static double F(double x, double a) => 1/(Math.Sqrt(Math.Pow(x,2)+a));
 
         static double SimpsonIntegral(double a, double b, int n)
         {
-            double h = (b - a) / n; 
+            double h = (b - a) / n;
             double sum = F(a, a) + F(b, a);
 
             for (int i = 1; i < n; i += 2)
@@ -34,17 +31,19 @@ namespace _5._3_V10
 
         static void Main(string[] args)
         {
-            
+
             double a, b, x, h, s;
             int i, n;
-            
+            ///
+            ////////Переспросить
+            ///
             Console.WriteLine("Введите начало промежутка");
             a = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите конец промежутка");
-            b = double.Parse(Console.ReadLine());
+            b = double.PositiveInfinity;
+            Console.WriteLine("Rонец промежутка {0}",b);
             Console.WriteLine("Введите N");
             n = int.Parse(Console.ReadLine());
-            Console.WriteLine("ЗНАЧЕНИЕ ИНТЕГРАЛА {0}", Math.Round(SimpsonIntegral(a,b,n),5));
+            Console.WriteLine("ЗНАЧЕНИЕ ИНТЕГРАЛА {0}", Math.Round(SimpsonIntegral(a, b, n), 5));
             Console.ReadKey();
 
 
